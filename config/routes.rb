41410ba1f8dde8bd925d2ad892a6149de23f2d6c
LeviCole <lckennedy@gmail.com>
2010-01-05 20:01:04 -0500

    adding root route

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ TestApp::Application.routes.draw do |map|
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "contacts#index"
 
   # See how all your routes lay out with "rake routes"
 
